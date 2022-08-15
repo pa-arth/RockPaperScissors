d96f685817e4ecc36057dce2d972a322d8807e21
Paarth <paarthguardian@gmail.com>
2022-08-15 17:18:00 -0500

    Add basic website design

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 function getComputerChoice () {
-    let index = Math.floor(Math.random() * 3) 
-    let arr = ["Rock", "Paper", "Scissors"]
-    return arr[index]
+    let index = Math.floor(Math.random() * 3) ;
+    let arr = ["Rock", "Paper", "Scissors"];
+    return arr[index];
 }
 
-console.log(getComputerChoice())
+function clicked () {
+    document.querySelector('.rock').style.outline = "1px solid blue"
+}
+
+let computerChoice; 
+let playerScore = 0;
+let computerScore = 0;
+// document.querySelector('.rock').style.outline = none;
+// document.querySelector('.rock').onclick = clicked();
+
+for (let i = 0; i < 5; ++i) {
+    
+    computerChoice = getComputerChoice();
+    console.log(computerChoice)
+    if (computerChoice == "Rock") {
+        document.querySelector('.crock').style.outline = "1px solid blue"
+    }
+    const playerPoints = document.querySelector("#playerScore")
+    console.log(playerPoints)
+    playerPoints.textContent = `Player score: ${playerScore}`
+    const compPoints = document.querySelector("#compScore")
+    console.log(compPoints)
+    compPoints.textContent = `Computer score: ${computerScore}`
+
+    
+}
